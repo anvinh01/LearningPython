7862ac161fc5477d091f7f79b256797b6e153b7e
anvinh01 <anvinh@gmx.de>
2021-05-26 17:22:02 +0200

    changes made in 01_variabless.py

--- 01_Variables.py
+++ 01_Variables.py
@@ -1,4 +1,14 @@
+is_string = "ist text oder eine Zeichenkette"
 
-# a string mit input
-a = int("5")
-print("dein input", a)
+is_integer = 5
+
+is_float = 5.5
+
+string_add = is_string + is_string
+print(string_add)
+
+string_add_int = is_string + str(is_integer)
+print(string_add_int)
+
+string_mul_int = is_string * is_integer
+print(string_mul_int)
